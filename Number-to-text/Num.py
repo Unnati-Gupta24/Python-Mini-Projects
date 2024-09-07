b9a8a767834cc2bc8a1896ed0cd8db85130059bf
Unnati gupta <guptaunnati031@gmail.com>
2024-09-07 16:06:12 +0530

    Create Num.py

---
+++ Number-to-text/Num.py
@@ -0,0 +1,18 @@
+output = ""
+phone = input("Phone: ")
+mapping_dict = {
+   "1": "One",
+   "2": "Two",
+   "3": "Three",
+   "4": "Four",
+   "5": "Five",
+   "6": "Six",
+   "7": "Seven",
+   "8": "Eight",
+   "9": "Nine",
+   "0": "Zero"
+}
+for ch in phone:
+   output += mapping_dict.get(ch," ")
+
+print(output)
